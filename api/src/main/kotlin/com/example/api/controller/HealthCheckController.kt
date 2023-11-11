eb79a23532765d7507d63fe936bfae270a39db2d
seung-00 <seung-00@naver.com>
2023-11-11 11:42:29 +0200

    feat: health check api 수정

--- api/src/main/kotlin/com/example/api/controller/HealthCheckController.kt
+++ api/src/main/kotlin/com/example/api/controller/HealthCheckController.kt
@@ -2,10 +2,12 @@ package com.example.api.controller
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["*"])
 @Tag(name = "헬스체크 API", description = "Swagger 테스트입니다")
 class HealthCheckController {
 
@@ -13,4 +15,3 @@ class HealthCheckController {
     @GetMapping("/healthCheck")
     fun healthCheck() = "OK"
 }
-
